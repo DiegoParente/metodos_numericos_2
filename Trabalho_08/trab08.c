420a7345a2630b08956892f0a9a5af42089b881c
DiegoParente <diego@diegoparente.com>
2013-05-20 04:14:36 +0000

    lances funcionando

--- Trabalho_08/trab08.c
+++ Trabalho_08/trab08.c
@@ -9,9 +9,36 @@ double f1(double x){
 }
 double f1_a = -1.0;
 double f1_b = 1.0;
+//cos(Pi x)/( Sqrt(1 -x) )
+double f2(double x){
+	return cos(M_PI*x)/sqrt(1.0 -x);
+}
+
 
 double exp_f1(double x){
-	return f1((1.0/2)*(f1_a+f1_b+(f1_b - f1_a)*tanh(x)))*(0.5)*(f1_b-f1_a)*(pow(cosh(x)*cosh(x), -1));
+	double k = (0.5)*(f1_a + f1_b + (f1_b - f1_a)*tanh(x));
+	double dkdx = (f1_b - f1_a)/2 	/ pow(cosh(x), 2);
+	return f1(k)*dkdx;
+}
+
+double simp_exp(double a, double b, double x, double(*f)(double)){
+	double k = (0.5)*(a + b + (b - a)*tanh(x));
+	double dkdx = (b - a)/2 	/ pow(cosh(x), 2);
+	return f(k)*dkdx;
+}
+
+double dbl_exp(double a, double b, double x, double (*f)(double)){
+	double k = 0.5* (a + b + (b - a)*(tanh(M_PI/2 *sinh(x))));
+	double dkdx = (b-a)*M_PI/4*cosh(x) /  pow(cosh(M_PI/2 * sinh(x)),2);
+	return f(k)*dkdx;
+}
+
+double exp_f2(double x){
+	return dbl_exp(-1, 1, x, f2);
+}
+
+double dex_f1(double x){
+	return dbl_exp(-1, 1, x, f1);
 }
 
 double extended_trapezoidal_rule_test_function(double x){
@@ -21,5 +48,8 @@ double extended_trapezoidal_rule_test_function(double x){
 int main(){
 	printf("%lf\n", extended_trapezoidal_rule(extended_trapezoidal_rule_test_function));
 	printf("%lf\n", extended_trapezoidal_rule(exp_f1));
+	printf("%lf\n", extended_trapezoidal_rule(exp_f2));
+	printf("%lf\n", extended_trapezoidal_rule(dex_f1));
+
 	return 0;
 }
